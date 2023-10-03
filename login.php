df9619153cea79aaf21bcca1030a25081f09fd73
tungnv <tungnv@kimnamgroup.vn>
2023-10-03 20:53:31 +0700

    bo_dn_fb_va_gg

--- login.php
+++ login.php
@@ -1,7 +1,5 @@
 <?php
 include __DIR__ . '/classes/Account.php';
-include __DIR__ . '/google-login/config.php';
-require __DIR__ . '/facebook-login/vendor/autoload.php';
 ?>
 
 <?php
@@ -24,91 +22,6 @@ if (isset($_SESSION['form_data'])) {
 }
 ?>
 
-
-<?php
-// Login Google 
-
-if (isset($_GET["code"])) {
-
-    $token = $google_client->fetchAccessTokenWithAuthCode($_GET["code"]);
-
-
-    if (!isset($token['error'])) {
-
-        $google_client->setAccessToken($token['access_token']);
-
-
-        $_SESSION['access_token'] = $token['access_token'];
-
-
-        $google_service = new Google_Service_Oauth2($google_client);
-
-
-        $data = $google_service->userinfo->get();
-
-        $login_google = $class->loginGoogle($data['given_name'], $data['family_name'], $data['email'], $data['picture']);
-    }
-}
-
-?>
-
-
-<?php
-//initialize facebook sdk
-$fb = new Facebook\Facebook([
-    'app_id' => '853879869280674',
-    // your app id
-    'app_secret' => 'bf3b03a3d98b50dcbfc373be12de4e61',
-    // your app secret
-    'default_graph_version' => 'v2.5',
-]);
-$helper = $fb->getRedirectLoginHelper();
-$permissions = ['email']; // optional
-try {
-    if (isset($_SESSION['facebook_access_token'])) {
-        $accessToken = $_SESSION['facebook_access_token'];
-    } else {
-        $accessToken = $helper->getAccessToken();
-    }
-} catch (Facebook\Exceptions\facebookResponseException $e) {
-    // When Graph returns an error
-    echo 'Graph returned an error: ' . $e->getMessage();
-    exit;
-} catch (Facebook\Exceptions\FacebookSDKException $e) {
-    // When validation fails or other local issues
-    echo 'Facebook SDK returned an error: ' . $e->getMessage();
-    exit;
-}
-if (isset($accessToken)) {
-    if (isset($_SESSION['facebook_access_token'])) {
-        $fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
-    } else {
-        // getting short-lived access token
-        $_SESSION['facebook_access_token'] = (string) $accessToken;
-        // OAuth 2.0 client handler
-        $oAuth2Client = $fb->getOAuth2Client();
-        // Exchanges a short-lived access token for a long-lived one
-        $longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken($_SESSION['facebook_access_token']);
-        $_SESSION['facebook_access_token'] = (string) $longLivedAccessToken;
-        // setting default access token to be used in script
-        $fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
-        $profile_request = $fb->get('/me?fields=name,first_name,last_name,email');
-        $requestPicture = $fb->get('/me/picture?redirect=false&height=200'); //getting user picture
-        $picture = $requestPicture->getGraphUser();
-        $profile = $profile_request->getGraphUser();
-        $fbid = $profile->getProperty('id'); // To Get Facebook ID
-        $fbfullname = $profile->getProperty('name'); // To Get Facebook full name
-        $fbemail = $profile->getProperty('email'); //  To Get Facebook email
-        $fbpic =  $picture['url'];
-        # save the user nformation in session variable
-        $login_facebook = $class->loginFacebook( $fbid, $fbfullname, $fbemail, $fbpic);
-    }
-} else {
-    // replace  website URL same as added in the developers.Facebook.com/apps e.g. if you used http instead of https and used            
-    $loginUrl = $helper->getLoginUrl('https://datn.io.vn/login.php', $permissions);
-}
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -163,14 +76,6 @@ if (isset($accessToken)) {
                                 <input type="submit" name="signin" id="signin" class="form-submit" value="Đăng nhập" />
                             </div>
                         </form>
-                        <div class="social-login">
-                            <span class="social-label">Hoặc đăng nhập với</span>
-                            <ul class="socials">
-                                <li><a href="<?= $loginUrl ?>"><i class="display-flex-center zmdi zmdi-facebook"></i></a></li>
-                                <li><a href="<?= $google_client->createAuthUrl() ?>"><i
-                                            class="display-flex-center zmdi zmdi-google"></i></a></li>
-                            </ul>
-                        </div>
                     </div>
                 </div>
             </div>
